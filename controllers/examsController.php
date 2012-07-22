4b20a1490d851866dd1970f339fedb3c9410acbb
Mohamed Ait Slimane <mohamed.aitslimane@gmail.com>
2012-07-22 11:55:23 +0100

    vue et controleur pour les examens

---
+++ controllers/examsController.php
@@ -0,0 +1,39 @@
+<?php
+require_once('dao/examDAO_Impl.php');
+
+class examsController{
+	private $dao;
+	private $action;
+	
+	public function __construct(){
+		$this->dao = new examDAO_Impl;
+		$this->action = 'list';
+	}
+	
+	public function process(){
+		if(isset($_GET['ation'])){
+			$this->action = $_GET['action'];
+			switch($this->action){
+				case 'list':
+					if(isset($_GET['dt'])){
+						$exams = $this->dao->examByDate($_GET['dt']);
+					}elseif(isset($_GET['patient'])){
+						$exams = $this->dao->examByPatient($_GET['patient']);
+					}else{
+						$exams=NULL;
+					}
+					$this->loadView('exams', $exams);
+					break;
+				case 'add':
+					break;
+			}
+		}
+	}
+	
+	public function loadView($v, $data) {
+		$view = 'views/' . $v . '.php';
+		$consults = $data;
+		include ($view);
+	}
+}
+?>
---
+++ views/exams.php
@@ -0,0 +1,8 @@
+
+<div>
+	<form method="get" action="">
+		<label for="nompatient">Nom :</label> <input type="text" name="nompatient" id="nompatient" />
+		<input type="hidden" name="patient" id="patient" />
+		<label for="date">Date :</label> <input type="text" name="dt" id="dt" />
+	</form>
+</div>
