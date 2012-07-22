5674f5a89c190c61e86079e5946b02780c49aa13
Mohamed Ait Slimane <mohamed.aitslimane@gmail.com>
2012-07-22 11:20:05 +0100

    logic consultations

---
+++ classes/consultation.php
@@ -0,0 +1,46 @@
+<?php
+class Consultation{
+	private $idconsultation;
+	private $patient;
+	private $date_consult;
+	private $rapport;
+	
+	public function __construct($patien,$date,$rapport){
+		$this->patient = $patien;
+		$this->date_consult = $date;
+		$this->rapport = $rapport;
+	}
+	
+	public function setId($id){
+		$this->idconsultation = $id;
+	}
+	
+	public function getId(){
+		return $this->idconsultation;
+	}
+	
+	public function setPatient($id){
+		$this->patient = $id;
+	}
+	
+	public function getPatient(){
+		return $this->patient;
+	}
+	
+	public function setDateConsult($dt){
+		$this->date_consult = $dt;
+	}
+	
+	public function getDateConsult(){
+		return $this->date_consult;
+	}
+	
+	public function setRapport($rap){
+		$this->rapport = $rap;
+	}
+	
+	public function getRapport(){
+		return $this->rapport;
+	}
+}
+?>
