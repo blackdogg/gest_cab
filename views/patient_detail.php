36e08b6ead7c3a2089f5aaec444994a9e813966f
Mohamed Ait Slimane <mohamed.aitslimane@gmail.com>
2012-07-29 09:43:10 +0100

    modif vue patient_detail

--- views/patient_detail.php
+++ views/patient_detail.php
@@ -3,7 +3,7 @@
 <div id="patient_tool">
 	<div onclick="dt_cons('<?php echo $patients[0]['idpatient']; ?>')">Details des consultations</div>
 	<div onclick="dt_exams('<?php echo $patients[0]['idpatient']; ?>')">Details des examens</div>
-	<div onclick="dt_evo('<?php echo $patients[0]['idpatient']; ?>')">Evolutions</div>
+	<!--<div onclick="dt_evo('<?php echo $patients[0]['idpatient']; ?>')">Evolutions</div>-->
 </div>
 
 <div class="fiche">
@@ -12,6 +12,8 @@
 	<span class="label">Date de naissance :</span><span class="info"><?php echo $patients[0]['date_naissance']; ?></span>
 	<span class="label">Adresse :</span><span class="info"><?php echo $patients[0]['adresse']; ?></span>
 	<span class="label">Telephone :</span><span class="info"><?php echo $patients[0]['tel']; ?></span>
+	<span class="label">Antecedents familiaux :</span><span class="info"><?php echo $patients[0]['aicdf']; ?></span>
+	<span class="label">Antecedents personnels :</span><span class="info"><?php echo $patients[0]['aicdp']; ?></span>
 </div>
 
 <div id="detail_frm">
